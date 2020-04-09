32b12c1f41ea480f3c454f909d45e50bdb6f2126
Sabrina Sulong <tesmemepasla@gmail.com>
2020-04-09 13:45:57 +0800

    Implement Book class with getters to retrieve attribute values

---
+++ test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,32 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class BookTest {
+    public Book book;
+
+    @Before
+    public void beforeEach() {
+        book = new Book("Nineteen Eighty-Four", "George Orwell", 1949);
+    }
+
+    @Test
+    public void getTitleShouldGetBookTitle() {
+        assertThat(book.getTitle(), is(equalTo("Nineteen Eighty-Four")));
+    }
+
+    @Test
+    public void getAuthorShouldGetBookAuthor() {
+        assertThat(book.getAuthor(), is(equalTo("George Orwell")));
+    }
+
+    @Test
+    public void getPublicationYearShouldGetBookPublicationYear() {
+        assertThat(book.getPublicationYear(), is(equalTo(1949)));
+    }
+}

2f38fd0fc0bd3a6142c66fb3ae11cc2863c4082b
Sabrina Sulong <tesmemepasla@gmail.com>
2020-04-14 13:26:49 +0800

    Add Library.checkoutBook tests and refactor

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -1,11 +1,15 @@
 package com.twu.biblioteca;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
-import static org.mockito.Mockito.*;
 
 public class LibraryTest {
     Library library = new Library();
@@ -13,13 +17,27 @@ public class LibraryTest {
     private Book book2;
     private Book book3;
 
+    private PrintStream systemOut = System.out;
+    private ByteArrayOutputStream output;
+
     @Before
     public void beforeEach() {
-        book1 = mock(Book.class);
-        book2 = mock(Book.class);
-        book3 = mock(Book.class);
+        book1 = new Book("Book1", "Author1", 2001);
+        book2 = new Book("Book2", "Author2", 2002);
+        book3 = new Book("Book3", "Author3", 2003);
         library.addBook(book1);
         library.addBook(book2);
+    }
+
+    @Before
+    public void setup() {
+        output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+    }
+
+    @After
+    public void reset() {
+        System.setOut(systemOut);
     }
 
     @Test
@@ -39,7 +57,26 @@ public class LibraryTest {
     public void printBooksShouldPrintDetailsOfBooks() {
         library.printBooks();
 
-        verify(book1).printBook();
-        verify(book2).printBook();
+        assertThat(output.toString(),
+                containsString("Title | Author | Year of publication"));
+        assertThat(output.toString(),
+                both(containsString("Book1")).
+                and(containsString("Book2")));
+    }
+
+    @Test
+    public void checkoutBookShouldCheckoutBookIfValid() {
+        library.checkoutBook(book1.getTitle());
+
+        assertThat(book1.available, equalTo(false));
+    }
+
+    @Test
+    public void checkoutBookShouldReturnErrorMessageIfInvalid() {
+        library.checkoutBook("Non-existent Book");
+
+        assertThat(book1.available, equalTo(true));
+        assertThat(output.toString(), containsString(
+                "Sorry, that book is not available"));
     }
 }

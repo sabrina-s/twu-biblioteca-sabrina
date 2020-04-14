47e9af2e3bc4fbfd69d09acac8c8658e1be71e28
Sabrina Sulong <tesmemepasla@gmail.com>
2020-04-14 13:26:16 +0800

    Populate Library books before app execution
    To prevent re-population of books each time

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -20,14 +20,6 @@ public class BibliotecaApp {
 //        print welcome message
         printer.print(WELCOME_MESSAGE);
 
-//        populate library with books
-        Book book1 = new Book("Book1", "Author1", 2001);
-        Book book2 = new Book("Book2", "Author2", 2002);
-        Book book3 = new Book("Book3", "Author3", 2003);
-        library.addBook(book1);
-        library.addBook(book2);
-        library.addBook(book3);
-
 //        display menu
         menu.printOptions();
 
--- src/com/twu/biblioteca/Main.java
+++ src/com/twu/biblioteca/Main.java
@@ -2,11 +2,21 @@ package com.twu.biblioteca;
 
 public class Main {
     public static void main(String[] args) {
+        Printer printer = new Printer();
+        Library library = new Library();
+        Menu menu = new Menu();
+        InputReader inputReader = new InputReader();
+
+        // populate library with books
+        Book book1 = new Book("Book1", "Author1", 2001);
+        Book book2 = new Book("Book2", "Author2", 2002);
+        Book book3 = new Book("Book3", "Author3", 2003);
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+
         BibliotecaApp app = new BibliotecaApp(
-                new Printer(),
-                new Library(),
-                new Menu(),
-                new InputReader()
+                printer, library, menu, inputReader
         );
 
         boolean active = app.execute();
